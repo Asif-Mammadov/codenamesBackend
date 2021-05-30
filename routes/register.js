1d34df265f57e4b20d4fd84a5bf23e70b2e05e56
Asif Mammadov <asif.mammadov89@gmail.com>
2021-05-30 19:28:45 +0400

    fix repeated registration

--- routes/register.js
+++ routes/register.js
@@ -25,6 +25,20 @@ Router.post('/registered', function(req, res) {
             message: "Too short password."
         });
     } else {
+        var sql = 'SELECT * FROM User WHERE Email = ? OR Username= ?'
+        db.query(sql, [email, name_reg], function(err, rows){
+            if(err){
+                db.end();
+                return console.log(err);
+            }
+            if(rows.length){
+                db.end();
+                return res.json({
+                    success: 0,
+                    message: "User already exists"
+                });
+            }
+        })
         if(phone) {
             var post = {Username: name_reg, Email: email, Password: password, PhoneNumber:phone, CreationDate: new Date(),
                         Wins: 0, Draws: 0, Loses: 0, GuessedRightAsOperative: 0, GuessedWrongAsOperative: 0,
@@ -40,6 +54,7 @@ Router.post('/registered', function(req, res) {
         db.query(sql, post, (err, results) => {
             if(err) {
                 console.log(err);
+                db.end();
                 return res.json({
                     success: 0,
                     message: "Database connection error"
@@ -49,6 +64,7 @@ Router.post('/registered', function(req, res) {
             const jsontoken = sign({ result: results }, process.env.TOKEN_SECRET, {
                 expiresIn: "1h"
             });
+            db.end();
             return res.json({
                 success: 1,
                 message: "Successfully registered",
